5a7231012ee213fdaab332a8bbeaa88f31c9fff9
Maksymilian Wasilewski <maks.wazne@gmail.com>
2025-05-26 22:24:49 +0200

    Create 052.py

---
+++ codewars.com/052.py
@@ -0,0 +1,29 @@
+"""
+https://www.codewars.com/kata/55983863da40caa2c900004e/solutions/python
+
+Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+
+  12 ==> 21
+ 513 ==> 531
+2017 ==> 2071
+
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift, None in Rust):
+
+  9 ==> -1
+111 ==> -1
+531 ==> -1
+"""
+
+def next_bigger(n):
+    digits = list(str(n))
+    i = len(digits) - 2
+    while i >= 0 and digits[i] >= digits[i + 1]:
+        i -= 1
+    if i == -1:
+        return -1
+    j = len(digits) - 1
+    while digits[j] <= digits[i]:
+        j -= 1
+    digits[i], digits[j] = digits[j], digits[i]
+    digits[i + 1:] = sorted(digits[i + 1:])
+    return int(''.join(digits))
